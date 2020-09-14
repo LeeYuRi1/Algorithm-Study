7c89218d53440b64e96c784f919b86668597e767
LeeYuRi1 <dbf51561590@gmail.com>
2020-09-14 21:36:17 +0900

    chapter2-1 code24

---
+++ src/java_data_structure/chapter2_1/Code24_Class.java
@@ -0,0 +1,22 @@
+package java_data_structure.chapter2_1;
+
+public class Code24_Class {
+    public static void main(String[] args) {
+
+        Person1 first;
+        first = new Person1();
+
+        first.name = "John";
+        first.number = "1234243223";
+
+        System.out.println("First is " + first.name + " with number " + first.number);
+
+        Person1[] members = new Person1[100];
+        members[0] = first;
+        members[1] = new Person1();
+        members[1].name = "David";
+        members[1].number = "463464643";
+
+        System.out.println("Second is " + members[1].name + " with number " + members[1].number);
+    }
+}
---
+++ src/java_data_structure/chapter2_1/Person1.java
@@ -0,0 +1,6 @@
+package java_data_structure.chapter2_1;
+
+public class Person1 {
+    public String name;
+    public String number;
+}
