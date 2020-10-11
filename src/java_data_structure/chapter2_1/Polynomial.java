76d30c94ce6352ff36c83a39c8fb3dc9cfd75f38
LeeYuRi1 <dbf51561590@gmail.com>
2020-10-11 19:57:20 +0900

    [java data structure] chapter2-1 code6

---
+++ src/java_data_structure/chapter2_1/Polynomial.java
@@ -0,0 +1,8 @@
+package java_data_structure.chapter2_1;
+
+// 하나의 다항식을 표현
+public class Polynomial {
+    public char name;
+    public Term[] terms;
+    public int nTerms = 0;
+}
---
+++ src/java_data_structure/chapter2_1/Term.java
@@ -0,0 +1,7 @@
+package java_data_structure.chapter2_1;
+
+// 다항식을 구성하는 하나의 항을 표현
+public class Term {
+    public int coef; // 계수
+    public int expo; // 차수
+}
