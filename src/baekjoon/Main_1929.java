5f8c977335b96762fff98a089629c35f8f65cb41
LeeYuRi1 <dbf51561590@gmail.com>
2021-01-15 22:57:39 +0900

    [baekjoon] codeplus 수학 - 소수, 에라토스테네스의 체

--- src/baekjoon/Main_1929.java
+++ src/baekjoon/Main_1929.java
@@ -2,20 +2,25 @@ package baekjoon;
 
 import java.util.Scanner;
 
+//에라토스테네스의 체
 public class Main_1929 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-
-        for (int i = num1; i <= num2; i++) {
-            boolean isPrime = true;
-            if (i == 1) isPrime = false;
-            for (int j = 2; j * j <= i && isPrime; j++) {
-                if (i % j == 0) isPrime = false;
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        boolean check[] = new boolean[m + 1];
+        check[0] = check[1] = true;
+        for (int i = 2; i * i <= m; i++) {
+            if (!check[i]) {
+                for (int j = i + i; j <= m; j += i) {
+                    check[j] = true;
+                }
             }
-            if (isPrime) System.out.println(i);
         }
-
+        for (int i = n; i <= m; i++) {
+            if (check[i] == false) {
+                System.out.println(i);
+            }
+        }
     }
 }
--- src/baekjoon/Main_6588.java
+++ src/baekjoon/Main_6588.java
@@ -1,45 +1,40 @@
 package baekjoon;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main_6588 {
     public static final int MAX = 1000000;
-    static boolean[] isPrime = new boolean[MAX + 1];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Integer> num = new ArrayList<>();
-        boolean flag = false;
-        primeList();
+        boolean check[] = new boolean[MAX + 1]; //true: 지워진 것(소수가 아님), false: 지워지지 않음(소수)
+        ArrayList<Integer> prime = new ArrayList<>();
+
+        //에라토스테네스의 체
+        check[0] = check[1] = true;
+        for (int i = 2; i * i <= MAX; i++) {
+            if (!check[i]) {
+                prime.add(i);
+                for (int j = i + i; j <= MAX; j += i) {
+                    check[j] = true;
+                }
+            }
+        }
 
         while (true) {
+            boolean b = false;
             int n = sc.nextInt();
             if (n == 0) break;
-            num.add(n);
-        }
-
-        for (int n = 0; n < num.size(); n++) {
-            flag = false;
-            for (int m = 3; m <= num.get(n) / 2; m += 2)
-                if (!isPrime[m] && !isPrime[num.get(n) - m]) {
-                    System.out.println(num.get(n) + " = " + m + " + " + (num.get(n) - m));
-                    flag = true;
+            for (int i = 1; i < prime.size(); i++) {
+                int p = prime.get(i);
+                if (!check[n - p]) {
+                    System.out.println(n + " = " + p + " + " + (n - p));
+                    b = true;
                     break;
                 }
-            if (!flag) System.out.println("Goldbach's conjecture is wrong.");
+            }
+            if (!b) System.out.println("Goldbach's conjecture is wrong.");
         }
     }
-
-    // 에라토스테네스의 체
-    private static void primeList() {
-        isPrime[0] = true;
-        isPrime[1] = true;
-        for (int i = 2; i * i < MAX; i++)
-            if (!isPrime[i])
-                for (int j = i + i; j <= MAX; j += i)
-                    isPrime[j] = true;
-    }
-
 }
