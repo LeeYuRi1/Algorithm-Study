b089e88d51bd13083861eb13b678d618e04f68c4
LeeYuRi1 <dbf51561590@gmail.com>
2021-01-25 00:09:05 +0900

    [programmers] 완주하지 못한 선수

---
+++ src/programmers/level1/UnfinishedPlayer.java
@@ -0,0 +1,29 @@
+package programmers.level1;
+
+import java.util.HashMap;
+
+public class UnfinishedPlayer {
+    public static void main(String[] args) {
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+        System.out.println(solution(participant, completion));
+    }
+
+    static private String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String s : participant) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        for (String s : completion) {
+            map.put(s, map.get(s) - 1);
+        }
+
+        for (String s : map.keySet()) {
+            if (map.get(s) != 0)
+                answer = s;
+        }
+        return answer;
+    }
+}
