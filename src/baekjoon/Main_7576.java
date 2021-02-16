759b7b2dc83c8c58cc3152a3df0302dd03a78c05
LeeYuRi1 <dbf51561590@gmail.com>
2021-02-16 22:40:14 +0900

    [baekjoon] 7576 오류 수정

--- src/baekjoon/Main_7576.java
+++ src/baekjoon/Main_7576.java
@@ -28,7 +28,7 @@ public class Main_7576 {
 
         a = new int[n][m];
         dist = new int[n][m];
-        Queue q = new LinkedList();
+        Queue<Pair_7576> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 a[i][j] = sc.nextInt();
@@ -58,16 +58,18 @@ public class Main_7576 {
     }
 
     private static void bfs(Queue<Pair_7576> q, int n, int m) {
-        Pair_7576 p = q.remove();
-        int x = p.x;
-        int y = p.y;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (0 <= nx && nx < n && 0 <= ny && ny < y) {
-                if (a[nx][ny] == 0 && dist[nx][ny] == -1) {
-                    q.add(new Pair_7576(nx, ny));
-                    dist[nx][ny] = dist[x][y] + 1;
+        while (!q.isEmpty()) {
+            Pair_7576 p = q.remove();
+            int x = p.x;
+            int y = p.y;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                    if (a[nx][ny] == 0 && dist[nx][ny] == -1) {
+                        q.add(new Pair_7576(nx, ny));
+                        dist[nx][ny] = dist[x][y] + 1;
+                    }
                 }
             }
         }
