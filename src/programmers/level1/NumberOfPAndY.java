de13d2d341142a8a52048bdcb246d9e9023f8805
LeeYuRi1 <dbf51561590@gmail.com>
2021-02-19 03:20:13 +0900

    [programmers] 문제풀이

---
+++ src/programmers/level1/NumberOfPAndY.java
@@ -0,0 +1,24 @@
+package programmers.level1;
+
+// 문자열 내 p와 y의 개수
+public class NumberOfPAndY {
+    public static void main(String[] args) {
+        System.out.println(solution("pPoooyY"));
+        System.out.println(solution("Pyy"));
+    }
+
+    static boolean solution(String s) {
+        s = s.toLowerCase();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'p') {
+                count++;
+            }
+            if (c == 'y') {
+                count--;
+            }
+        }
+        return count == 0;
+    }
+}
