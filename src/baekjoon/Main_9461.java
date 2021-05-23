e08130fe0e372bd23fddfcf8d250ee89d1f84856
LeeYuRi1 <dbf51561590@gmail.com>
2021-05-24 00:03:19 +0900

    [baekjoon] 9461 수정

--- src/baekjoon/Main_9461.java
+++ src/baekjoon/Main_9461.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main_9461 {
-    private static int[] num = new int[101];
+    private static long[] num = new long[101];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        num[1] = 1;
+        num[2] = 1;
+        num[3] = 1;
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
             sb.append(find(n) + "\n");
@@ -17,10 +20,8 @@ public class Main_9461 {
         System.out.println(sb);
     }
 
-    private static int find(int n) {
-        if (n == 0) return 0;
-        if (n <= 3) return 1;
-        else if (num[n] != 0) return num[n];
+    private static long find(int n) {
+        if (num[n] != 0) return num[n];
         else num[n] = find(n - 2) + find(n - 3);
         return num[n];
     }
