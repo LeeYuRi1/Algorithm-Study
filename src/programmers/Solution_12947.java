56e1016f92304e8f48aa8b079c6a4ed225ae1721
LeeYuRi1 <dbf51561590@gmail.com>
2021-07-15 00:07:45 +0900

    [programmers] 하샤드수 다른 풀이

--- src/programmers/Solution_12947.java
+++ src/programmers/Solution_12947.java
@@ -4,18 +4,30 @@ package programmers;
 // x의 각 자릿수의 합으로 x가 나누어지는 양의 정수
 public class Solution_12947 {
     public static void main(String[] args) {
-        System.out.println(solution(10));
-        System.out.println(solution(12));
-        System.out.println(solution(11));
-        System.out.println(solution(13));
+        System.out.println(solution1(10));
+        System.out.println(solution1(12));
+        System.out.println(solution2(11));
+        System.out.println(solution2(13));
     }
 
-    public static boolean solution(int x) {
+    // x를 10으로 나누어 1의자리를 구하는 방식
+    public static boolean solution1(int x) {
         int sum = 0;
         int n = x;
         while (n > 0) {
-            sum += (n % 10);
+            sum += n % 10;
             n /= 10;
+        }
+        return x % sum == 0;
+    }
+
+    // 문자열로 바꾸어 각 자릿수를 변환하는 방식
+    public static boolean solution2(int x) {
+        String s = String.valueOf(x);
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i) - '0';
+//            sum += Character.getNumericValue(s.charAt(i));
         }
         return x % sum == 0;
     }
