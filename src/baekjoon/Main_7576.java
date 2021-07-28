0e3a94fe0561dde173541165307c0e09aa13bffb
LeeYuRi1 <dbf51561590@gmail.com>
2021-07-28 19:40:30 +0900

    [baekjoon] bfs 7576

--- src/baekjoon/Main_7576.java
+++ src/baekjoon/Main_7576.java
@@ -1,8 +1,9 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 class Pair_7576 {
     int x;
@@ -15,23 +16,23 @@ class Pair_7576 {
 }
 
 public class Main_7576 {
-
     static private int[][] a;
     static private int[][] dist;
     static private int[] dx = {0, 0, 1, -1};
     static private int[] dy = {1, -1, 0, 0};
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s1 = br.readLine().split(" ");
+        int m = Integer.parseInt(s1[0]);
+        int n = Integer.parseInt(s1[1]);
         a = new int[n][m];
         dist = new int[n][m];
         Queue<Pair_7576> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
+            String[] s2 = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
-                a[i][j] = sc.nextInt();
+                a[i][j] = Integer.parseInt(s2[j]);
                 dist[i][j] = -1;
                 if (a[i][j] == 1) {
                     q.add(new Pair_7576(i, j));
@@ -40,6 +41,7 @@ public class Main_7576 {
             }
         }
         bfs(q, n, m);
+        // 얼마나 걸렸는지 알기 위해 dist의 최대값을 구함
         int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -65,12 +67,10 @@ public class Main_7576 {
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    if (a[nx][ny] == 0 && dist[nx][ny] == -1) {
-                        q.add(new Pair_7576(nx, ny));
-                        dist[nx][ny] = dist[x][y] + 1;
-                    }
-                }
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (a[nx][ny] != 0 || dist[nx][ny] != -1) continue;
+                q.add(new Pair_7576(nx, ny));
+                dist[nx][ny] = dist[x][y] + 1;
             }
         }
     }
