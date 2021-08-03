a7871e475c864a945bf1e1fd7d686a6052b9ced7
LeeYuRi1 <dbf51561590@gmail.com>
2021-08-03 21:28:01 +0900

    [baekjoon] 다익스트라 11779

--- src/baekjoon/Main_11779.java
+++ src/baekjoon/Main_11779.java
@@ -7,80 +7,83 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Stack;
 
-class Edge_11779 implements Comparable<Edge_11779> {
-    int to;
-    int cost;
+class Pair_11779 implements Comparable<Pair_11779> {
+    int v;
+    int w;
 
-    Edge_11779(int to, int cost) {
-        this.to = to;
-        this.cost = cost;
+    public Pair_11779(int v, int w) {
+        this.v = v;
+        this.w = w;
     }
 
     @Override
-    public int compareTo(Edge_11779 o) {
-        return this.cost - o.cost;
+    public int compareTo(Pair_11779 o) {
+        return this.w - o.w;
     }
 }
 
 public class Main_11779 {
+    private static ArrayList<Pair_11779>[] arr;
+    private static int[] dist;
+    private static boolean[] check;
+    private static int[] path;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-
-        ArrayList<Edge_11779>[] a = new ArrayList[n + 1];
-        int[] d = new int[n + 1];
-        int[] v = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            a[i] = new ArrayList<>();
+        arr = new ArrayList[n + 1];
+        dist = new int[n + 1];
+        check = new boolean[n + 1];
+        path = new int[n + 1];
+        for (int i = 1; i <= n; i++) arr[i] = new ArrayList<>();
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(path, -1);
+        while (m-- > 0) {
+            String[] s2 = br.readLine().split(" ");
+            int a = Integer.parseInt(s2[0]);
+            int b = Integer.parseInt(s2[1]);
+            int c = Integer.parseInt(s2[2]);
+            arr[a].add(new Pair_11779(b, c));
         }
-        for (int i = 0; i < m; i++) {
-            String[] input = br.readLine().split(" ");
-            int x = Integer.parseInt(input[0]);
-            int y = Integer.parseInt(input[1]);
-            int w = Integer.parseInt(input[2]);
-            a[x].add(new Edge_11779(y, w));
-        }
-        String[] input = br.readLine().split(" ");
-        int start = Integer.parseInt(input[0]);
-        int end = Integer.parseInt(input[1]);
-
-        dijkstra(a, d, v, start);
-        print(d, v, end);
+        String[] s3 = br.readLine().split(" ");
+        int start = Integer.parseInt(s3[0]);
+        int end = Integer.parseInt(s3[1]);
+        dijkstra(start);
+        print(end);
     }
 
-    private static void dijkstra(ArrayList<Edge_11779>[] a, int[] d, int[] v, int start) {
-        PriorityQueue<Edge_11779> queue = new PriorityQueue<>();
-        queue.add(new Edge_11779(start, 0));
-        Arrays.fill(d, Integer.MAX_VALUE);
-        d[start] = 0;
-        v[start] = 0;
-        while (!queue.isEmpty()) {
-            Edge_11779 e = queue.poll();
-            int end = e.to;
-            for (Edge_11779 edge : a[end]) {
-                if (d[edge.to] > d[end] + edge.cost) {
-                    d[edge.to] = d[end] + edge.cost;
-                    queue.add(new Edge_11779(edge.to, d[edge.to]));
-                    v[edge.to] = end;
+    private static void dijkstra(int start) {
+        PriorityQueue<Pair_11779> pq = new PriorityQueue<>();
+        pq.offer(new Pair_11779(start, 0));
+        dist[start] = 0;
+        while (!pq.isEmpty()) {
+            Pair_11779 p = pq.poll();
+            int x = p.v;
+            if (check[x]) continue; // 이 부분 없으면 시간초과!
+            check[x] = true;
+            for (Pair_11779 y : arr[x]) {
+                if (dist[y.v] > dist[x] + y.w) {
+                    dist[y.v] = dist[x] + y.w;
+                    path[y.v] = x;
+                    pq.offer(new Pair_11779(y.v, dist[y.v]));
                 }
             }
         }
     }
 
-    private static void print(int[] d, int[] v, int end) {
-        Stack<Integer> stack = new Stack<>();
+    // 역추적 - 스택 사용
+    private static void print(int end) {
         StringBuilder sb = new StringBuilder();
-        int x = end;
-        while (x != 0) {
-            stack.push(x);
-            x = v[x];
+        sb.append(dist[end] + "\n");
+        Stack<Integer> stack = new Stack<>();
+        while (end != -1) {
+            stack.push(end);
+            end = path[end];
         }
-        sb.append(d[end] + "\n");
         sb.append(stack.size() + "\n");
-        while (!stack.empty()) {
-            sb.append(stack.peek() + " ");
-            stack.pop();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop() + " ");
         }
         System.out.println(sb);
     }
