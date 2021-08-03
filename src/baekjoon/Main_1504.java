0b02f2f33588463d9f6259012968132c85b427c9
LeeYuRi1 <dbf51561590@gmail.com>
2021-08-03 18:04:01 +0900

    [baekjoon] 1504 수정

--- src/baekjoon/Main_1504.java
+++ src/baekjoon/Main_1504.java
@@ -22,13 +22,17 @@ class Edge_1504 implements Comparable<Edge_1504> {
 }
 
 public class Main_1504 {
+    private static ArrayList<Edge_1504>[] a;
+    private static int n;
+    private static int inf = 200000000;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
+        n = Integer.parseInt(input[0]);
         int m = Integer.parseInt(input[1]);
 
-        ArrayList<Edge_1504>[] a = new ArrayList[n + 1];
+        a = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             a[i] = new ArrayList<>();
         }
@@ -44,26 +48,26 @@ public class Main_1504 {
         int v1 = Integer.parseInt(input3[0]);
         int v2 = Integer.parseInt(input3[1]);
 
-        int ans1 = dijkstra(a, n, 1, v1) + dijkstra(a, n, v1, v2) + dijkstra(a, n, v2, n);
-        int ans2 = dijkstra(a, n, 1, v2) + dijkstra(a, n, v2, v1) + dijkstra(a, n, v1, n);
+        int ans1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, n);
+        int ans2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, n);
         int ans = Math.min(ans1, ans2);
-        if (ans < 0 || m == 0) System.out.println(-1);
+        if (ans >= inf) System.out.println(-1);
         else System.out.println(ans);
     }
 
-    private static int dijkstra(ArrayList<Edge_1504>[] a, int n, int start, int end) {
+    private static int dijkstra(int start, int end) {
         PriorityQueue<Edge_1504> queue = new PriorityQueue<>();
-        queue.add(new Edge_1504(start, 0));
+        queue.offer(new Edge_1504(start, 0));
         int[] d = new int[n + 1];
-        Arrays.fill(d, Integer.MAX_VALUE);
+        Arrays.fill(d, inf);
         d[start] = 0;
         while (!queue.isEmpty()) {
             Edge_1504 e = queue.poll();
             int x = e.to;
-            for (Edge_1504 edge : a[x]) {
-                if (d[edge.to] > d[x] + edge.cost) {
-                    d[edge.to] = d[x] + edge.cost;
-                    queue.add(new Edge_1504(edge.to, d[edge.to]));
+            for (Edge_1504 y : a[x]) {
+                if (d[y.to] > d[x] + y.cost) {
+                    d[y.to] = d[x] + y.cost;
+                    queue.offer(new Edge_1504(y.to, d[y.to]));
                 }
             }
         }
