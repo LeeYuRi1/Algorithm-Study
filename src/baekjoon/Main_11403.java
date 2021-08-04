4ce20a2f8c12fbb2f6e77929ca5a6767220152a0
LeeYuRi1 <dbf51561590@gmail.com>
2021-08-04 18:21:36 +0900

    [baekjoon] 플로이드 11403

--- src/baekjoon/Main_11403.java
+++ src/baekjoon/Main_11403.java
@@ -7,22 +7,22 @@ public class Main_11403 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] a = new int[n][n];
+        int[][] g = new int[n][n];
         int inf = 100000;
         for (int i = 0; i < n; i++) {
             String[] input = br.readLine().split(" ");
             for (int j = 0; j < n; j++) {
                 int x = Integer.parseInt(input[j]);
-                if (x == 0) a[i][j] = inf;
-                else a[i][j] = Integer.parseInt(input[j]);
+                if (x == 0) g[i][j] = inf;
+                else g[i][j] = x;
             }
         }
 
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (a[i][j] > a[i][k] + a[k][j]) {
-                        a[i][j] = a[i][k] + a[k][j];
+                    if (g[i][j] > g[i][k] + g[k][j]) {
+                        g[i][j] = g[i][k] + g[k][j];
                     }
                 }
             }
@@ -31,7 +31,7 @@ public class Main_11403 {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (a[i][j] == inf) sb.append("0 ");
+                if (g[i][j] == inf) sb.append("0 ");
                 else sb.append("1 ");
             }
             sb.append("\n");
