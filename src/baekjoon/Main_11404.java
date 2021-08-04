aa58ee09dcb27f7bf43ddc9d42d1537f3c4efcad
LeeYuRi1 <dbf51561590@gmail.com>
2021-08-04 21:29:59 +0900

    [baekjoon] 플로이드 11404

--- src/baekjoon/Main_11404.java
+++ src/baekjoon/Main_11404.java
@@ -2,33 +2,29 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main_11404 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        int[][] a = new int[n][n];
+        int[][] g = new int[n][n];
         int inf = 100000000;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) a[i][j] = 0;
-                else a[i][j] = inf;
-            }
-        }
+        for (int[] i : g) Arrays.fill(i, inf);
         for (int i = 0; i < m; i++) {
             String[] input = br.readLine().split(" ");
-            int x = Integer.parseInt(input[0]);
-            int y = Integer.parseInt(input[1]);
-            int w = Integer.parseInt(input[2]);
-            a[x - 1][y - 1] = Math.min(a[x - 1][y - 1], w);
+            int a = Integer.parseInt(input[0]) - 1;
+            int b = Integer.parseInt(input[1]) - 1;
+            int c = Integer.parseInt(input[2]);
+            g[a][b] = Math.min(g[a][b], c);
         }
 
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (a[i][j] > a[i][k] + a[k][j]) {
-                        a[i][j] = a[i][k] + a[k][j];
+                    if (g[i][j] > g[i][k] + g[k][j]) {
+                        g[i][j] = g[i][k] + g[k][j];
                     }
                 }
             }
@@ -37,8 +33,8 @@ public class Main_11404 {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (a[i][j] == inf) sb.append("0 ");
-                else sb.append(a[i][j] + " ");
+                if (g[i][j] == inf || i == j) sb.append("0 ");
+                else sb.append(g[i][j] + " ");
             }
             sb.append("\n");
         }
