4f10da560d76761130e257c162d67e8f190a5407
LeeYuRi1 <dbf51561590@gmail.com>
2021-09-02 22:03:53 +0900

    [baekjoon] 11286

--- src/baekjoon/Main_11286.java
+++ src/baekjoon/Main_11286.java
@@ -1,32 +1,30 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main_11286 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                if (Math.abs(o1) < Math.abs(o2)) {
-                    return -1;
-                } else if (Math.abs(o1) == Math.abs(o2)) {
-                    return o1 - o2;
-                } else {
-                    return 1;
-                }
+                if (Math.abs(o1) == Math.abs(o2)) return o1 - o2;
+                return Math.abs(o1) - Math.abs(o2);
             }
         });
+
         StringBuilder sb = new StringBuilder();
         while (n-- > 0) {
             int x = Integer.parseInt(br.readLine());
-            if (x != 0) pq.add(x);
-            else {
-                if (pq.isEmpty()) sb.append(0 + "\n");
+            if (x != 0) {
+                pq.offer(x);
+            } else {
+                if (pq.isEmpty()) sb.append("0 \n");
                 else sb.append(pq.poll() + "\n");
             }
         }
