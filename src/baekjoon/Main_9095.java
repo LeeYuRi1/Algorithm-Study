49479343873aef980a1057db9255a85262352c1a
LeeYuRi1 <dbf51561590@gmail.com>
2021-09-21 00:17:12 +0900

    [baekjoon] 9095

--- src/baekjoon/Main_9095.java
+++ src/baekjoon/Main_9095.java
@@ -1,30 +1,29 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 // d[n] = n을 1,2,3의 조합으로 나타내는 방법의 수
 // d[n] = d[n-1] + d[n-2] + d[n-3]
 // 시간복잡도: O(N)
 public class Main_9095 {
-
-    private static int[] d = new int[11];
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            System.out.println(combination(n));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= dp.length - 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
 
-    }
-
-    private static int combination(int n) {
-        if (n == 1 || n == 2) return n;
-        if (n == 3) return 4;
-        if (d[n] > 0) return d[n];
-        d[n] = combination(n - 1) + combination(n - 2) + combination(n - 3);
-        return d[n];
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n] + "\n");
+        }
+        System.out.println(sb);
     }
 }
