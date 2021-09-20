3c64728ded23f9a04864375d3b6d5d4ba94df59d
LeeYuRi1 <dbf51561590@gmail.com>
2021-09-21 00:16:32 +0900

    [baekjoon] 1463

--- src/baekjoon/Main_1463.java
+++ src/baekjoon/Main_1463.java
@@ -1,6 +1,8 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 // d[n] = n을 1로 만드는데 필요한 연산의 최소값
 // 1. n을 3으로 나누었을 때 1로 만드는데 필요한 연산 횟수: d[n/3] + 1
@@ -8,32 +10,16 @@ import java.util.Scanner;
 // 3. n을 1로 뺄 때 1로 만드는데 필요한 연산 횟수: d[n-1] + 1
 // d[n] = min(1, 2, 3)
 public class Main_1463 {
-
-    private static int[] d;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        d = new int[n+1];
-        System.out.println(go(n));
-    }
-
-    //시간복잡도: O(N) = N(채워야하는 d 배열의 칸의 개수) * O(1)(한칸을 채우기 위한 복잡도)
-    private static int go(int n) {
-        if (n == 1) return 0;
-        if (d[n] > 0) return d[n];
-        //n => n-1
-        d[n] = go(n - 1) + 1;
-        //n => n/2
-        if (n % 2 == 0) {
-            int temp = go(n / 2) + 1;
-            if (d[n] > temp) d[n] = temp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
         }
-        //n => n/3
-        if (n % 3 == 0) {
-            int temp = go(n / 3) + 1;
-            if (d[n] > temp) d[n] = temp;
-        }
-        return d[n];
+        System.out.println(dp[n]);
     }
 }
