3a5fe1eb2f2450ee601af40a15b9d289ba7c8ea2
LeeYuRi1 <dbf51561590@gmail.com>
2021-09-21 23:24:41 +0900

    [baekjoon] 2193

--- src/baekjoon/Main_2193.java
+++ src/baekjoon/Main_2193.java
@@ -1,25 +1,23 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 // d[n] = n자리 이친수
 // 1. n번째 자리에 0이 오는 경우, 2. n번째 자리에 1이 오는 경우
 // d[n] = d[n-1] + d[n-2]
 public class Main_2193 {
-
-    private static long[] d;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        d = new long[n+1];
-        System.out.println(pinary(n));
-    }
-
-    private static long pinary(int n) {
-        if (n<=1) return n;
-        if (d[n] > 0) return d[n];
-        d[n] = pinary(n-1) + pinary(n-2);
-        return d[n];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
     }
 }
