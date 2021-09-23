56f17865134727e9f71701866c3f0edcba573af4
LeeYuRi1 <dbf51561590@gmail.com>
2021-09-23 21:08:10 +0900

    [baekjoon] 14499

--- src/baekjoon/Main_14499.java
+++ src/baekjoon/Main_14499.java
@@ -1,75 +1,81 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main_14499 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s1 = br.readLine().split(" ");
-        int n = Integer.parseInt(s1[0]);
-        int m = Integer.parseInt(s1[1]);
-        int x = Integer.parseInt(s1[2]);
-        int y = Integer.parseInt(s1[3]);
-        int k = Integer.parseInt(s1[4]);
-        int[][] map = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            String[] s2 = br.readLine().split(" ");
-            for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(s2[j]);
-            }
-        }
-        String[] s3 = br.readLine().split(" ");
+    private static int[] dice = new int[7];
+    private static int[][] map;
+    private static int x, y;
+    private static int[] dx = {0, 0, -1, 1}; //우좌상하
+    private static int[] dy = {1, -1, 0, 0};
+    private static StringBuilder sb = new StringBuilder();
 
-        int[] dx = {0, 0, -1, 1}; // 동, 서, 북, 남
-        int[] dy = {1, -1, 0, 0};
-        int[] dice = new int[7];
-        for (int i = 0; i < k; i++) {
-            int d = Integer.parseInt(s3[i]);
-            int nx = x + dx[d - 1];
-            int ny = y + dy[d - 1];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                move(d, dice);
-                if (map[nx][ny] == 0) {
-                    map[nx][ny] = dice[6];
-                } else {
-                    dice[6] = map[nx][ny];
-                    map[nx][ny] = 0;
-                }
-                x = nx;
-                y = ny;
-                System.out.println(dice[1]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input1 = br.readLine().split(" ");
+        int n = Integer.parseInt(input1[0]);
+        int m = Integer.parseInt(input1[1]);
+        x = Integer.parseInt(input1[2]);
+        y = Integer.parseInt(input1[3]);
+        int k = Integer.parseInt(input1[4]);
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String[] input2 = br.readLine().split(" ");
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(input2[j]);
             }
         }
+        String[] input3 = br.readLine().split(" ");
+        for (int i = 0; i < k; i++) {
+            int d = Integer.parseInt(input3[i]) - 1;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            x = nx;
+            y = ny;
+            move(d + 1);
+        }
+        System.out.println(sb);
     }
 
-    private static void move(int d, int[] dice) {
-        int temp = dice[1];
+    private static void move(int d) {
         switch (d) {
-            case 1:
-                dice[1] = dice[4];
-                dice[4] = dice[6];
-                dice[6] = dice[3];
-                dice[3] = temp;
+            case 1: // 오른쪽
+                rollDice(1, 4, 6, 3);
+                copy();
                 break;
-            case 2:
-                dice[1] = dice[3];
-                dice[3] = dice[6];
-                dice[6] = dice[4];
-                dice[4] = temp;
+            case 2: // 왼쪽
+                rollDice(1, 3, 6, 4);
+                copy();
                 break;
-            case 3:
-                dice[1] = dice[5];
-                dice[5] = dice[6];
-                dice[6] = dice[2];
-                dice[2] = temp;
+            case 3: // 위쪽
+                rollDice(1, 5, 6, 2);
+                copy();
                 break;
-            case 4:
-                dice[1] = dice[2];
-                dice[2] = dice[6];
-                dice[6] = dice[5];
-                dice[5] = temp;
+            case 4: //아래쪽
+                rollDice(1, 2, 6, 5);
+                copy();
                 break;
+        }
+        sb.append(dice[1] + "\n");
+    }
+
+    private static void rollDice(int n1, int n2, int n3, int n4) {
+        int temp = dice[n1];
+        dice[n1] = dice[n2];
+        dice[n2] = dice[n3];
+        dice[n3] = dice[n4];
+        dice[n4] = temp;
+    }
+
+    private static void copy() {
+        if (map[x][y] == 0) {
+            map[x][y] = dice[6];
+        } else {
+            dice[6] = map[x][y];
+            map[x][y] = 0;
         }
     }
 }
