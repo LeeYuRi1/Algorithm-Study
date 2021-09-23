3823a192c77b779bf77f4bccd446823daa4ea1a1
LeeYuRi1 <dbf51561590@gmail.com>
2021-09-23 19:33:23 +0900

    [baekjoon] 3190

--- src/baekjoon/Main_3190.java
+++ src/baekjoon/Main_3190.java
@@ -1,67 +1,59 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Point_3190 {
-    int x;
-    int y;
-
-    Point_3190(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main_3190 {
-    public static void main(String[] args) throws Exception {
+    private static int[][] board;
+    private static int n, d = 0;
+    private static int[] dx = {0, 1, 0, -1}; //우하좌상
+    private static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         int k = Integer.parseInt(br.readLine());
-        int[][] dummy = new int[n][n];
-        for (int i = 0; i < k; i++) {
-            String[] s = br.readLine().split(" ");
-            int x = Integer.parseInt(s[0]);
-            int y = Integer.parseInt(s[1]);
-            dummy[x - 1][y - 1] = 1;
+        board = new int[n][n];
+        while (k-- > 0) {
+            String[] input = br.readLine().split(" ");
+            board[Integer.parseInt(input[0]) - 1][Integer.parseInt(input[1]) - 1] = 1;
         }
         int l = Integer.parseInt(br.readLine());
-        String[] dir = new String[10001];
-        Arrays.fill(dir, "");
-        for (int i = 0; i < l; i++) {
-            String[] s = br.readLine().split(" ");
-            dir[Integer.parseInt(s[0])] = s[1];
+        char[] dir = new char[10001];
+        while (l-- > 0) {
+            String[] input = br.readLine().split(" ");
+            dir[Integer.parseInt(input[0])] = input[1].charAt(0);
         }
+        System.out.println(move(dir));
+    }
 
-        int time = 0;
-        Queue<Point_3190> queue = new LinkedList<>();
-        queue.add(new Point_3190(0, 0));
-        int[] dx = {0, 1, 0, -1}; // 우 하 좌 상
-        int[] dy = {1, 0, -1, 0};
+    private static int move(char[] dir) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        board[0][0] = 2;
         int nx = 0;
         int ny = 0;
-        int d = 0;
+        int count = 0;
         while (true) {
-            if (dir[time].equals("L")) d = (d + 3) % 4;
-            else if (dir[time].equals("D")) d = (d + 1) % 4;
-            time++;
+            if (dir[count] == 'L') d = (d + 3) % 4;
+            if (dir[count] == 'D') d = (d + 1) % 4;
+            count++;
+
             nx += dx[d];
             ny += dy[d];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                queue.add(new Point_3190(nx, ny));
-                if (dummy[nx][ny] == 2) break;
-                if (dummy[nx][ny] == 1) {
-                    dummy[nx][ny] = 0;
-                } else {
-                    Point_3190 p = queue.poll();
-                    dummy[p.x][p.y] = 0;
-                }
-                dummy[nx][ny] = 2;
-            } else break;
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) return count; // 벽과 부딪힘
+            if (board[nx][ny] == 2) return count; // 자기몸과 부딪힘
+            if (board[nx][ny] == 1) { //사과 있으면
+                board[nx][ny] = 0;
+            } else { //사과 없으면
+                int[] p = queue.poll();
+                board[p[0]][p[1]] = 0;
+            }
+            queue.offer(new int[]{nx, ny});
+            board[nx][ny] = 2;
         }
-        System.out.println(time);
     }
 }
