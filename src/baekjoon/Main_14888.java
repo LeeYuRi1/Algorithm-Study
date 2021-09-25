157b845b7153bb2d027490ef9ecb87e166b0f6ce
LeeYuRi1 <dbf51561590@gmail.com>
2021-09-25 20:48:39 +0900

    [baekjoon] 14888

--- src/baekjoon/Main_14888.java
+++ src/baekjoon/Main_14888.java
@@ -1,61 +1,66 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main_14888 {
-    private static int max = -1000000001;
-    private static int min = 1000000001;
+    private static int[] num;
+    private static int[] op;
+    private static int[] arr;
+    private static boolean[] visited;
+    private static int max = Integer.MIN_VALUE;
+    private static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String[] s1 = br.readLine().split(" ");
-        String[] s2 = br.readLine().split(" ");
-
-        int[] num = new int[n];
-        int[] operator = new int[n - 1];
-        int[] result = new int[n - 1];
-        boolean[] visit = new boolean[n - 1];
-        int index = 0;
-
-        for (int i = 0; i < n; i++) num[i] = Integer.parseInt(s1[i]);
+        String[] input1 = br.readLine().split(" ");
+        String[] input2 = br.readLine().split(" ");
+        num = new int[n];
+        op = new int[n - 1];
+        arr = new int[n - 1];
+        visited = new boolean[n - 1];
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(input1[i]);
+        }
+        int opIndex = 0;
         for (int i = 0; i < 4; i++) {
-            int m = Integer.parseInt(s2[i]);
-            while (m-- > 0) {
-                operator[index] = i;
-                index++;
+            int count = Integer.parseInt(input2[i]);
+            while (count-- > 0) {
+                op[opIndex] = i;
+                opIndex++;
             }
         }
-        dfs(num, operator, result, visit, 0, n);
-        System.out.println(max);
-        System.out.println(min);
+        selectOperator(0, n - 1);
+        System.out.println(max + "\n" + min);
     }
 
-    private static void dfs(int[] num, int[] operator, int[] result, boolean[] visit, int index, int n) {
-        if (index == n - 1) {
-            calculate(num, result);
+    private static void selectOperator(int depth, int n) {
+        if (depth == n) {
+            calculation();
             return;
         }
-        for (int i = 0; i < n - 1; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                result[index] = operator[i];
-                dfs(num, operator, result, visit, index + 1, n);
-                visit[i] = false;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = op[i];
+                selectOperator(depth + 1, n);
+                visited[i] = false;
             }
         }
     }
 
-    private static void calculate(int[] num, int[] result) {
-        int ans = num[0];
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] == 0) ans += num[i + 1];
-            else if (result[i] == 1) ans -= num[i + 1];
-            else if (result[i] == 2) ans *= num[i + 1];
-            else ans /= num[i + 1];
+    private static void calculation() {
+        int result = num[0];
+        for (int i = 0; i < arr.length; i++) {
+            int n = arr[i];
+            if (n == 0) result += num[i + 1];
+            else if (n == 1) result -= num[i + 1];
+            else if (n == 2) result *= num[i + 1];
+            else result /= num[i + 1];
         }
-        if (ans > max) max = ans;
-        if (ans < min) min = ans;
+        max = Math.max(max, result);
+        min = Math.min(min, result);
     }
 }
