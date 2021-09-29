74066008be07df661f41f2ed7b660dbea2fc62bb
LeeYuRi1 <dbf51561590@gmail.com>
2021-09-29 19:31:25 +0900

    [baekjoon] 15686

--- src/baekjoon/Main_15686.java
+++ src/baekjoon/Main_15686.java
@@ -1,61 +1,75 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+
+class Pair_15686 {
+    int x;
+    int y;
+
+    public Pair_15686(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class Main_15686 {
-    private static ArrayList<int[]> house;
-    private static ArrayList<int[]> chicken;
-    private static int[][] arr;
-    private static int ans = Integer.MAX_VALUE;
+    private static int m, ans = Integer.MAX_VALUE;
+    private static int[][] city;
+    private static List<Pair_15686> house;
+    private static List<Pair_15686> chicken;
+    private static Pair_15686[] arr;
+    private static boolean[] visited;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s1 = br.readLine().split(" ");
-        int n = Integer.parseInt(s1[0]);
-        int m = Integer.parseInt(s1[1]);
+        String[] input1 = br.readLine().split(" ");
+        int n = Integer.parseInt(input1[0]);
+        m = Integer.parseInt(input1[1]);
+        city = new int[n][n];
         house = new ArrayList<>();
         chicken = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            String[] s2 = br.readLine().split(" ");
+            String[] input2 = br.readLine().split(" ");
             for (int j = 0; j < n; j++) {
-                if (Integer.parseInt(s2[j]) == 1) house.add(new int[]{i, j});
-                if (Integer.parseInt(s2[j]) == 2) chicken.add(new int[]{i, j});
+                city[i][j] = Integer.parseInt(input2[j]);
+                if (city[i][j] == 1) house.add(new Pair_15686(i, j));
+                if (city[i][j] == 2) chicken.add(new Pair_15686(i, j));
             }
         }
-        arr = new int[m][2];
-        boolean[] visit = new boolean[chicken.size()];
-        backtracking(visit, 0, 0, m);
+        arr = new Pair_15686[m];
+        visited = new boolean[chicken.size()];
+        selectChicken(0, 0);
         System.out.println(ans);
     }
 
-    private static void backtracking(boolean[] visit, int start, int index, int m) {
-        if (index == m) {
+    private static void selectChicken(int depth, int start) {
+        if (depth == m) {
             minDistance();
             return;
         }
         for (int i = start; i < chicken.size(); i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                arr[index][0] = chicken.get(i)[0];
-                arr[index][1] = chicken.get(i)[1];
-                backtracking(visit, i + 1, index + 1, m);
-                visit[i] = false;
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = chicken.get(i);
+                selectChicken(depth + 1, i);
+                visited[i] = false;
             }
         }
     }
 
     private static void minDistance() {
-        int result = 0;
+        int sum = 0;
         for (int i = 0; i < house.size(); i++) {
             int min = Integer.MAX_VALUE;
-            int[] h = house.get(i);
             for (int j = 0; j < arr.length; j++) {
-                min = Math.min(min, Math.abs(h[0] - arr[j][0]) + Math.abs(h[1] - arr[j][1]));
+                min = Math.min(min, Math.abs(house.get(i).x - arr[j].x) + Math.abs(house.get(i).y - arr[j].y));
             }
-            result += min;
+            sum += min;
         }
-        ans = Math.min(ans, result);
+        ans = Math.min(ans, sum);
     }
 }
