994a0f2cdab44b579cea45f31662659929ef4352
LeeYuRi1 <dbf51561590@gmail.com>
2021-12-01 23:11:30 +0900

    11399

--- src/baekjoon/Main_11399.java
+++ src/baekjoon/Main_11399.java
@@ -9,18 +9,18 @@ public class Main_11399 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String[] s = br.readLine().split(" ");
+        String[] input = br.readLine().split(" ");
         int[] time = new int[n];
         for (int i = 0; i < n; i++) {
-            time[i] = Integer.parseInt(s[i]);
+            time[i] = Integer.parseInt(input[i]);
         }
         Arrays.sort(time);
-        int sum = 0;
-        int result = 0;
+        int ans = 0;
+        int wait = 0;
         for (int i = 0; i < n; i++) {
-            sum += time[i];
-            result += sum;
+            ans += wait + time[i];
+            wait += time[i];
         }
-        System.out.println(result);
+        System.out.println(ans);
     }
 }
