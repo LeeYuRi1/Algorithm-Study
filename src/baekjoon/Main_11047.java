d5b9bac1532c737abd1f1ebacac63d9624b33497
LeeYuRi1 <dbf51561590@gmail.com>
2021-12-01 23:11:16 +0900

    [baekjoon] 11047

--- src/baekjoon/Main_11047.java
+++ src/baekjoon/Main_11047.java
@@ -1,10 +1,11 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main_11047 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
@@ -13,12 +14,13 @@ public class Main_11047 {
         for (int i = 0; i < n; i++) {
             coin[i] = Integer.parseInt(br.readLine());
         }
-
-        int result = 0;
+        int ans = 0;
         for (int i = n - 1; i >= 0; i--) {
-            result += k / coin[i];
+            if (k < coin[i]) continue;
+            if (k == 0) break;
+            ans += k / coin[i];
             k %= coin[i];
         }
-        System.out.println(result);
+        System.out.println(ans);
     }
 }
