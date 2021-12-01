ab61f0a2bb883b49b8ecda1b6e446969ec7570cd
LeeYuRi1 <dbf51561590@gmail.com>
2021-12-01 23:11:58 +0900

    [baekjoon] 1931

--- src/baekjoon/Main_1931.java
+++ src/baekjoon/Main_1931.java
@@ -1,36 +1,42 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
+
+class Pair_1931 implements Comparable<Pair_1931> {
+    int start;
+    int end;
+
+    public Pair_1931(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int compareTo(Pair_1931 o) {
+        if (this.end == o.end) return this.start - o.start;
+        return this.end - o.end;
+    }
+}
 
 public class Main_1931 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
+        Pair_1931[] meeting = new Pair_1931[n];
         for (int i = 0; i < n; i++) {
-            String[] s = br.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(s[0]);
-            arr[i][1] = Integer.parseInt(s[1]);
+            String[] input = br.readLine().split(" ");
+            meeting[i] = new Pair_1931(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
         }
-
-        // 끝나는 시간으로 정렬
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[1] == o2[1]) return Integer.compare(o1[0], o2[0]);
-                else return Integer.compare(o1[1], o2[1]);
-            }
-        });
-
-        int ans = 1;
-        int end = arr[0][1];
-        for (int i = 1; i < n; i++) {
-            if (arr[i][0] >= end) {
-                end = arr[i][1];
+        Arrays.sort(meeting);
+        int time = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (meeting[i].start >= time) {
                 ans++;
+                time = meeting[i].end;
             }
         }
         System.out.println(ans);
