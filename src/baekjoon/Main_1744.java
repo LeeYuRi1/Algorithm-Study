b78c50125aaa7cf7e9fd53e5a269a7797e6f6e4c
LeeYuRi1 <dbf51561590@gmail.com>
2021-12-02 23:20:39 +0900

    [baekjoon] 1744

--- src/baekjoon/Main_1744.java
+++ src/baekjoon/Main_1744.java
@@ -14,42 +14,26 @@ public class Main_1744 {
         List<Integer> plus = new ArrayList<>();
         List<Integer> minus = new ArrayList<>();
         int zero = 0;
+        int one = 0;
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if (num > 0) plus.add(num);
-            else if (num < 0) minus.add(num);
+            int m = Integer.parseInt(br.readLine());
+            if (m == 1) one++;
+            else if (m > 0) plus.add(m);
+            else if (m < 0) minus.add(m);
             else zero++;
         }
+        if (plus.size() % 2 == 1) plus.add(1);
+        if (minus.size() % 2 == 1) minus.add(zero > 0 ? 0 : 1);
         Collections.sort(plus);
         Collections.sort(minus, Collections.reverseOrder());
 
-        int resultPlus = 0;
-        int start = 0;
-        if (plus.size() % 2 != 0) {
-            resultPlus += plus.get(0);
-            start = 1;
+        int ans = one;
+        for (int i = 0; i < plus.size(); i += 2) {
+            ans += plus.get(i) * plus.get(i + 1);
         }
-        for (int i = start; i < plus.size(); i += 2) {
-            if (plus.get(i) * plus.get(i + 1) > plus.get(i) + plus.get(i + 1)) {
-                resultPlus += plus.get(i) * plus.get(i + 1);
-            } else {
-                resultPlus += plus.get(i) + plus.get(i + 1);
-            }
+        for (int i = 0; i < minus.size(); i += 2) {
+            ans += minus.get(i) * minus.get(i + 1);
         }
-
-        int resultMinus = 0;
-        start = 0;
-        if (minus.size() % 2 != 0) {
-            if (zero == 0) resultMinus += minus.get(0);
-            start = 1;
-        }
-        for (int i = start; i < minus.size(); i += 2) {
-            if (minus.get(i) * minus.get(i + 1) > minus.get(i) + minus.get(i + 1)) {
-                resultMinus += minus.get(i) * minus.get(i + 1);
-            } else {
-                resultMinus += minus.get(i) + minus.get(i + 1);
-            }
-        }
-        System.out.println(resultPlus + resultMinus);
+        System.out.println(ans);
     }
 }
