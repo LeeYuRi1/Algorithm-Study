8cd10091654987be18a501bc9b00dc7f9f4da52e
LeeYuRi1 <dbf51561590@gmail.com>
2021-12-20 21:56:41 +0900

    [baekjoon] 2110

--- src/baekjoon/Main_2110.java
+++ src/baekjoon/Main_2110.java
@@ -1,45 +1,46 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main_2110 {
-    public static void main(String[] args) throws Exception {
+    private static int[] house;
+    private static int c;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int c = Integer.parseInt(input[1]);
-        List<Integer> list = new ArrayList<>();
+        String[] input1 = br.readLine().split(" ");
+        int n = Integer.parseInt(input1[0]);
+        c = Integer.parseInt(input1[1]);
+        house = new int[n];
         for (int i = 0; i < n; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+            house[i] = Integer.parseInt(br.readLine());
         }
-        Collections.sort(list);
-        System.out.println(find(list, c));
+        Arrays.sort(house);
+        System.out.println(find());
     }
 
-    private static int find(List<Integer> list, int c) {
-        int min = 1;
-        int max = list.get(list.size() - 1) - list.get(0);
-        int mid = 0;
+    // 가장 인접한 두 공유기 사이의 거리
+    // 커질수록 공유기의 개수 작아짐
+    private static long find() {
+        long min = 1, max = house[house.length - 1] - house[0], mid;
         while (min <= max) {
             mid = (min + max) / 2;
-            if (check(list, mid, c)) min = mid + 1;
+            if (check(mid)) min = mid + 1; // 공유기 개수가 c보다 같거나 크면 공유기 사이의 거리를 크게 만들어줌
             else max = mid - 1;
         }
         return max;
     }
 
-    private static boolean check(List<Integer> list, int mid, int c) { // mid: 가장 인접한 거리
-        int count = 1; // 공유기 설치 개수
-        int end = list.get(0);
-        for (int i : list) {
-            if (i - end >= mid) {
-                count++;
-                end = i;
-            }
+    private static boolean check(long mid) {
+        int count = 1;
+        int pre = house[0];
+        for (int i : house) {
+            if (i - pre < mid) continue;
+            pre = i;
+            count++;
         }
         return count >= c;
     }
