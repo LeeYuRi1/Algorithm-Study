302ec70e18256e92e482b02f35449f2c2539f684
LeeYuRi1 <dbf51561590@gmail.com>
2021-12-23 05:21:27 +0900

    [baekjoon] 1939

--- src/baekjoon/Main_1939.java
+++ src/baekjoon/Main_1939.java
@@ -1,72 +1,76 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
-class Edge {
-    public int v;
-    public int w;
+class Pair_1939 {
+    int next;
+    int w;
 
-    public Edge(int v, int w) {
-        this.v = v;
+    public Pair_1939(int next, int w) {
+        this.next = next;
         this.w = w;
     }
 }
 
 public class Main_1939 {
+    private static List<Pair_1939>[] bridge;
+    private static int n, start, end;
 
-    private static List<Edge>[] list;
-    private static boolean[] check;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-        list = new ArrayList[n + 1];
-        int max = 0;
+        String[] input1 = br.readLine().split(" ");
+        n = Integer.parseInt(input1[0]);
+        int m = Integer.parseInt(input1[1]);
+        bridge = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
-            list[i] = new ArrayList<>();
+            bridge[i] = new ArrayList<>();
         }
-        for (int i = 1; i <= m; i++) {
-            String[] s = br.readLine().split(" ");
-            int a = Integer.parseInt(s[0]);
-            int b = Integer.parseInt(s[1]);
-            int c = Integer.parseInt(s[2]);
+        int max = 0;
+        while (m-- > 0) {
+            String[] input2 = br.readLine().split(" ");
+            int a = Integer.parseInt(input2[0]);
+            int b = Integer.parseInt(input2[1]);
+            int c = Integer.parseInt(input2[2]);
+            bridge[a].add(new Pair_1939(b, c));
+            bridge[b].add(new Pair_1939(a, c));
             max = Math.max(max, c);
-            list[a].add(new Edge(b, c));
-            list[b].add(new Edge(a, c));
         }
-        String[] s = br.readLine().split(" ");
-        System.out.println(find(Integer.parseInt(s[0]), Integer.parseInt(s[1]), n, max));
+        String[] input3 = br.readLine().split(" ");
+        start = Integer.parseInt(input3[0]);
+        end = Integer.parseInt(input3[1]);
+        System.out.println(find(max));
     }
 
-    private static int find(int start, int end, int n, int m) {
-        int min = 1;
-        int max = m;
-        int ans = 0;
+    // w의 최대값 구하기
+    // w가 가능하면 w를 증가시키고, w가 불가능하면 w를 감소시킴
+    private static int find(int maxW) {
+        int min = 1, max = maxW, mid;
         while (min <= max) {
-            check = new boolean[n + 1];
-            int mid = (min + max) / 2;
-            if (dfs(start, end, mid)) {
-                ans = mid;
-                min = mid + 1;
-            } else {
-                max = mid - 1;
-            }
+            mid = (min + max) / 2;
+            if (bfs(mid)) min = mid + 1;
+            else max = mid - 1;
         }
-        return ans;
+        return max;
     }
 
-    private static boolean dfs(int start, int end, int mid) {
-        if (check[start]) return false;
-        check[start] = true;
-        if (start == end) return true;
-        for (Edge e : list[start]) {
-            if (mid <= e.w) {
-                if (dfs(e.v, end, mid)) return true;
+    private static boolean bfs(int w) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[n + 1];
+        queue.offer(start);
+        visited[start] = true;
+        while (!queue.isEmpty()) {
+            int p = queue.poll();
+            if (p == end) return true;
+            for (Pair_1939 pair : bridge[p]) {
+                if (visited[pair.next] || pair.w < w) continue;
+                queue.offer(pair.next);
+                visited[pair.next] = true;
             }
         }
         return false;
