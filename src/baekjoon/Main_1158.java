be66d5c43df242421fa504df65f1e6d33a9abb2a
이유리 <dbf5156@naver.com>
2022-01-24 21:00:53 +0900

    [baekjoon] 1158

--- src/baekjoon/Main_1158.java
+++ src/baekjoon/Main_1158.java
@@ -1,34 +1,31 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Main_1158 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        Queue queue = new LinkedList();
-
-        for (int i = 1; i < n + 1; i++) {
-            queue.add(i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
         }
 
-        System.out.print("<");
-        int count = 1;
-        while (!queue.isEmpty()) {
-            if (count == k) {
-                System.out.print(queue.remove());
-                count = 1;
-                if (queue.size() > 0)
-                    System.out.print(", ");
-
-            } else {
-                queue.add(queue.remove());
-                count++;
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while (queue.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                queue.offer(queue.poll());
             }
+            sb.append(queue.poll()).append(", ");
         }
-        System.out.print(">");
+        sb.append(queue.poll()).append(">");
+        System.out.println(sb);
     }
 }
