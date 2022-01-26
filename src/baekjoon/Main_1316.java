11312e650d13b57e1cb94a3471407ab1cb6578ff
이유리 <dbf5156@naver.com>
2022-01-26 22:30:15 +0900

    [baekjoon] 1316

--- src/baekjoon/Main_1316.java
+++ src/baekjoon/Main_1316.java
@@ -1,31 +1,29 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class Main_1316 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int count = 0;
+        int ans = 0;
         while (n-- > 0) {
-            String s = br.readLine();
-            if (groupWord(s)) count++;
+            if (check(br.readLine())) ans++;
         }
-        System.out.println(count);
+        System.out.println(ans);
     }
 
-    private static boolean groupWord(String s) {
-        String word = "";
-        for (int i = 0; i < s.length(); i++) {
-            String w = s.charAt(i) + "";
-            if (word.contains(w)) {
-                if (!(word.charAt(word.length() - 1) + "").equals(w)) {
-                    return false;
-                }
-            } else {
-                word += w;
-            }
+    private static boolean check(String str) {
+        HashSet<Character> set = new HashSet<>();
+        set.add(str.charAt(0));
+        for (int i = 1; i < str.length(); i++) {
+            char prev = str.charAt(i - 1);
+            char cur = str.charAt(i);
+            if (prev != cur && set.contains(cur)) return false; // 이전이랑 다르고 이미 나왔었다면 그룹단어가 아님
+            set.add(cur);
         }
         return true;
     }
