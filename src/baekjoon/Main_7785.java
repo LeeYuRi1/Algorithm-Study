b07757edd7d0a5325b41b6e693beaaa6772efad7
이유리 <dbf5156@naver.com>
2022-01-26 23:03:20 +0900

    [baekjoon] 7785

--- src/baekjoon/Main_7785.java
+++ src/baekjoon/Main_7785.java
@@ -3,24 +3,25 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Main_7785 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         HashSet<String> set = new HashSet<>();
+        StringTokenizer st;
         while (n-- > 0) {
-            String[] input = br.readLine().split(" ");
-            if (input[1].equals("enter")) set.add(input[0]);
-            else set.remove(input[0]);
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String record = st.nextToken();
+            if (record.equals("enter")) set.add(name);
+            else set.remove(name);
         }
         List<String> ans = new ArrayList<>(set);
         ans.sort(Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
         for (String s : ans) sb.append(s).append("\n");
         System.out.println(sb);
     }
