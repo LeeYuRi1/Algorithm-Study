3818398f5d9d74e01d7a70cbe36092cd35e0ab7e
이유리 <dbf5156@naver.com>
2022-02-07 17:42:46 +0900

    [baekjoon] 17478

--- src/baekjoon/Main_17478.java
+++ src/baekjoon/Main_17478.java
@@ -1,35 +1,32 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 소요 시간 : 76 ms
+// 메모리 사용량 : 12004 kb
 public class Main_17478 {
+    private static StringBuilder sb = new StringBuilder();
 
-    static private StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String s = "";
         sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
-        print(n, s);
+        recursive(n, "");
         System.out.println(sb);
     }
 
-    private static void print(int n, String s) {
+    private static void recursive(int n, String str) {
+        sb.append(str + "\"재귀함수가 뭔가요?\"\n");
         if (n == 0) {
-            sb.append(s + "\"재귀함수가 뭔가요?\"\n" +
-                    s + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n" +
-                    s + "라고 답변하였지.\n");
+            sb.append(str + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n" + str + "라고 답변하였지.\n");
             return;
         }
-        sb.append(s + "\"재귀함수가 뭔가요?\"\n" +
-                s + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" +
-                s + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" +
-                s + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
-        s += "____";
-        print(n - 1, s);
-        s = s.substring(0, s.length() - 4);
-        sb.append(s + "라고 답변하였지.\n");
+        sb.append(str + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" +
+                str + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" +
+                str + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+        recursive(n - 1, str + "____");
+        sb.append(str + "라고 답변하였지.\n");
     }
 }
