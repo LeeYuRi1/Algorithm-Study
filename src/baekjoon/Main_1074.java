b1611ff968e36c4152612fe6556144494dcbd12d
이유리 <dbf5156@naver.com>
2022-02-15 23:31:04 +0900

    [baekjoon] 1074

--- src/baekjoon/Main_1074.java
+++ src/baekjoon/Main_1074.java
@@ -1,42 +1,34 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+//소요 시간 : 80 ms
+//메모리 사용량 : 11516 kb
 public class Main_1074 {
-    private static int ans = 0;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        System.out.println(print((int) Math.pow(2, Integer.parseInt(input[0])), Integer.parseInt(input[1]), Integer.parseInt(input[2])));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        findNum(1 << N, r, c, 0);
     }
 
-    private static int print(int n, int x, int y) {
+    private static void findNum(int n, int x, int y, int count) {
         if (n == 1) {
-            return ans;
-        } else {
-            int l = n * n / 4;
-            if (x < n / 2) {
-                if (y < n / 2) {
-                    //왼쪽 위
-                    return print(n / 2, x, y);
-                } else {
-                    //오른쪽 위
-                    ans += l;
-                    return print(n / 2, x, y - n / 2);
-                }
-            } else {
-                if (y < n / 2) {
-                    //왼쪽 아래
-                    ans += l * 2;
-                    return print(n / 2, x - n / 2, y);
-                } else {
-                    //오른쪽 아래
-                    ans += l * 3;
-                    return print(n / 2, x - n / 2, y - n / 2);
-                }
-            }
+            System.out.println(count);
+            return;
         }
+        boolean top = (x < n / 2) ? true : false;
+        boolean left = (y < n / 2) ? true : false;
+
+        int m = n * n / 4;
+        if (top && left) findNum(n / 2, x, y, count);
+        else if (top && !left) findNum(n / 2, x, y - n / 2, count + m);
+        else if (!top && left) findNum(n / 2, x - n / 2, y, count + 2 * m);
+        else findNum(n / 2, x - n / 2, y - n / 2, count + 3 * m);
     }
 }
