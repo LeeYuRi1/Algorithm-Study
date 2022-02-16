1f3be2e854eef796afafc3cad9fbb12cb4860c8d
이유리 <dbf5156@naver.com>
2022-02-16 23:22:15 +0900

    [baekjoon] 2839

--- src/baekjoon/Main_2839.java
+++ src/baekjoon/Main_2839.java
@@ -1,26 +1,28 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+//소요 시간 : 76 ms
+//메모리 사용량 : 11456 kb
 public class Main_2839 {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int answer = 0;
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int ans = 0;
         while (true) {
             if (n < 0) {
-                System.out.println(-1);
+                ans = -1;
                 break;
             }
             if (n % 5 == 0) {
-                answer += n / 5;
-                System.out.println(answer);
+                ans += n / 5;
                 break;
             }
             n -= 3;
-            answer++;
+            ans++;
         }
+        System.out.println(ans);
     }
 }
