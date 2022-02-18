ff13e533a3deafdf2f43220210f01bcdfcf81eed
이유리 <dbf5156@naver.com>
2022-02-18 23:02:33 +0900

    [baekjoon] 15683

--- src/baekjoon/Main_15683.java
+++ src/baekjoon/Main_15683.java
@@ -1,15 +1,20 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-class Point_15683 {
+//소요 시간 : 320 ms
+//메모리 사용량 : 40772 kb
+class Cctv {
     int x;
     int y;
     int num;
 
-    public Point_15683(int x, int y, int num) {
+    public Cctv(int x, int y, int num) {
         this.x = x;
         this.y = y;
         this.num = num;
@@ -17,132 +22,89 @@ class Point_15683 {
 }
 
 public class Main_15683 {
-    private static int n, m;
-    private static int[][] area;
-    private static int[][] temp;
-    private static ArrayList<Point_15683> cctv = new ArrayList<>();
-    private static int[] arr;
-    private static int ans = Integer.MAX_VALUE;
+    private static int N, M, ans = Integer.MAX_VALUE, zeroSum = 0, sum;
+    private static int area[][], dir[];
+    private static boolean[][] visited;
+    private static List<Cctv> cctvList = new ArrayList<>();
+    private static int[] dx = {-1, 0, 1, 0}; // 상좌하우
+    private static int[] dy = {0, -1, 0, 1};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s1 = br.readLine().split(" ");
-        n = Integer.parseInt(s1[0]);
-        m = Integer.parseInt(s1[1]);
-        area = new int[n][m];
-        temp = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            String[] s2 = br.readLine().split(" ");
-            for (int j = 0; j < m; j++) {
-                area[i][j] = Integer.parseInt(s2[j]);
-                if (area[i][j] > 0 && area[i][j] < 6) cctv.add(new Point_15683(i, j, area[i][j]));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        area = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                area[i][j] = Integer.parseInt(st.nextToken());
+                if (area[i][j] == 0) zeroSum++;
+                else if (area[i][j] < 6) cctvList.add(new Cctv(i, j, area[i][j]));
             }
         }
-        arr = new int[cctv.size()];
-        backtraking(0);
+        dir = new int[cctvList.size()];
+        permutation(0);
         System.out.println(ans);
     }
 
-    // 4방향에 대해 백트래킹으로 모든 경우 구하기
-    private static void backtraking(int depth) {
-        if (depth == cctv.size()) {
-            minBlindSpot();
+    private static void permutation(int cnt) {
+        if (cnt == cctvList.size()) {
+            visited = new boolean[N][M];
+            int count = 0;
+            for (int i = 0; i < cctvList.size(); i++) {
+                count += getArea(cctvList.get(i), dir[i]);
+            }
+            ans = Math.min(ans, zeroSum - count);
             return;
         }
-        for (int i = 1; i <= 4; i++) {
-            arr[depth] = i;
-            backtraking(depth + 1);
+        for (int i = 0; i < 4; i++) {
+            dir[cnt] = i;
+            permutation(cnt + 1);
         }
     }
 
-    // 정한 방향에 대해 사각지대 크기 구하기
-    private static void minBlindSpot() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                temp[i][j] = area[i][j];
-            }
+    private static int getArea(Cctv cctv, int d) {
+        sum = 0;
+        switch (cctv.num) {
+            case 1:
+                check(cctv, d);
+                break;
+            case 2:
+                check(cctv, d);
+                check(cctv, (d + 2) % 4);
+                break;
+            case 3:
+                check(cctv, d);
+                check(cctv, (d + 1) % 4);
+                break;
+            case 4:
+                check(cctv, d);
+                check(cctv, (d + 1) % 4);
+                check(cctv, (d + 3) % 4);
+                break;
+            case 5:
+                check(cctv, 0);
+                check(cctv, 1);
+                check(cctv, 2);
+                check(cctv, 3);
+                break;
         }
-        for (int i = 0; i < cctv.size(); i++) {
-            int x = cctv.get(i).x;
-            int y = cctv.get(i).y;
-            int cctvNum = cctv.get(i).num;
-            int dir = arr[i];
-            switch (cctvNum) {
-                case 1:
-                    if (dir == 1) check(x, y, 0, 1); //오른쪽
-                    else if (dir == 2) check(x, y, 1, 0); //아래
-                    else if (dir == 3) check(x, y, 0, -1); //왼쪽
-                    else check(x, y, -1, 0); //위
-                    break;
-                case 2:
-                    if (dir >= 2) {
-                        check(x, y, 0, 1); //오른쪽
-                        check(x, y, 0, -1); //왼쪽
-                    } else {
-                        check(x, y, 1, 0); //아래
-                        check(x, y, -1, 0); //위
-                    }
-                    break;
-                case 3:
-                    if (dir == 1) {
-                        check(x, y, -1, 0); //위
-                        check(x, y, 0, 1); //오른쪽
-                    } else if (dir == 2) {
-                        check(x, y, 0, 1); //오른쪽
-                        check(x, y, 1, 0); //아래
-                    } else if (dir == 3) {
-                        check(x, y, 1, 0); //아래
-                        check(x, y, 0, -1); //왼쪽
-                    } else {
-                        check(x, y, 0, -1); //왼쪽
-                        check(x, y, -1, 0); //위
-                    }
-                    break;
-                case 4:
-                    if (dir == 1) {
-                        check(x, y, 0, -1); //왼쪽
-                        check(x, y, -1, 0); //위
-                        check(x, y, 0, 1); //오른쪽
-                    } else if (dir == 2) {
-                        check(x, y, -1, 0); //위
-                        check(x, y, 0, 1); //오른쪽
-                        check(x, y, 1, 0); //아래
-                    } else if (dir == 3) {
-                        check(x, y, 0, 1); //오른쪽
-                        check(x, y, 1, 0); //아래
-                        check(x, y, 0, -1); //왼쪽
-                    } else {
-                        check(x, y, 1, 0); //아래
-                        check(x, y, 0, -1); //왼쪽
-                        check(x, y, -1, 0); //위
-                    }
-                    break;
-                case 5:
-                    // 모든 방향
-                    check(x, y, 0, 1);
-                    check(x, y, 1, 0);
-                    check(x, y, 0, -1);
-                    check(x, y, -1, 0);
-                    break;
-            }
-        }
-        //사각지대 최소값 구하기
+        return sum;
+    }
+
+    private static void check(Cctv cctv, int d) {
+        int x = cctv.x;
+        int y = cctv.y;
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (temp[i][j] == 0) count++;
-            }
-        }
-        ans = Math.min(ans, count);
-    }
-
-    // cctv가 감시할 수 있는 곳 체크하기
-    private static void check(int x, int y, int dx, int dy) {
         while (true) {
-            x += dx;
-            y += dy;
-            if (x < 0 || x >= n || y < 0 || y >= m || temp[x][y] == 6) break;
-            temp[x][y] = 7;
+            x += dx[d];
+            y += dy[d];
+            if (x < 0 || x >= N || y < 0 || y >= M || area[x][y] == 6) break;
+            if (visited[x][y]) continue;
+            if (area[x][y] == 0) count++;
+            visited[x][y] = true;
         }
+        sum += count;
     }
 }
