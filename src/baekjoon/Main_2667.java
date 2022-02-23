f7c510124640407d84d2f0073c20e95cab50020a
이유리 <dbf5156@naver.com>
2022-02-23 18:32:24 +0900

    [baekjoon] 2667

--- src/baekjoon/Main_2667.java
+++ src/baekjoon/Main_2667.java
@@ -3,70 +3,49 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-// 플러드 필
-// 연결요소를 찾는 것과 유사
-class Pair_2667 {
-    int x;
-    int y;
-
-    Pair_2667(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main_2667 {
-
-    static private int[][] a;
-    static private boolean[][] check;
-    static private int[] dx = {0, 0, 1, -1};
-    static private int[] dy = {1, -1, 0, 0};
+    private static int N, count;
+    private static char[][] area;
+    private static boolean[][] visited;
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        a = new int[n][n];
-        check = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            String[] s = br.readLine().split("");
-            for (int j = 0; j < n; j++) {
-                a[i][j] = Integer.parseInt(s[j]);
-            }
+        N = Integer.parseInt(br.readLine());
+        area = new char[N][];
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            area[i] = br.readLine().toCharArray();
         }
 
-        ArrayList<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (a[i][j] == 1 && !check[i][j]) { // 정점이고 아직 방문하지 않으면 bfs 호출
-                    ans.add(bfs(i, j, n));
-                }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (visited[i][j] || area[i][j] == '0') continue;
+                count = 0;
+                dfs(i, j);
+                ans.add(count);
             }
         }
-
         Collections.sort(ans);
         System.out.println(ans.size());
         for (int i : ans) System.out.println(i);
     }
 
-    private static int bfs(int x, int y, int n) {
-        Queue<Pair_2667> q = new LinkedList<>();
-        q.offer(new Pair_2667(x, y));
-        check[x][y] = true;
-        int count = 1;
-        while (!q.isEmpty()) {
-            Pair_2667 p = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue; //범위를 벗어난다면 넘어가기
-                if (a[nx][ny] == 0 || check[nx][ny]) continue; // 간선이 없거나 이미 방문했다면 넘어가기
-                q.add(new Pair_2667(nx, ny));
-                check[nx][ny] = true;
-                count++;
-            }
+    private static void dfs(int x, int y) {
+        visited[x][y] = true;
+        count++;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            if (visited[nx][ny] || area[nx][ny] == '0') continue;
+            dfs(nx, ny);
         }
-        return count;
     }
 }
