d9fa629a11a7c318efc9eb4cf5fdd81149e72920
이유리 <dbf5156@naver.com>
2022-02-23 18:32:33 +0900

    [baekjoon] 2178

--- src/baekjoon/Main_2178.java
+++ src/baekjoon/Main_2178.java
@@ -5,63 +5,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 // dfs로 문제를 풀 수 없음. bfs 사용 (단계별로 진행)
 // 모든 가중치가 1이면 최단거리 알고리즘을 bfs로 구할 수 있음
-class Pair_2178 {
-    int x;
-    int y;
-
-    Pair_2178(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main_2178 {
-
-    static private int[][] a;
-    static private int[][] dist;
-    static private int[] dx = {0, 0, 1, -1};
-    static private int[] dy = {1, -1, 0, 0};
+    private static int N, M;
+    private static char[][] area;
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        int n = Integer.parseInt(s[0]);
-        int m = Integer.parseInt(s[1]);
-
-        a = new int[n][m];
-        dist = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            String[] s1 = br.readLine().split("");
-            for (int j = 0; j < m; j++) {
-                a[i][j] = Integer.parseInt(s1[j]);
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        area = new char[N][];
+        for (int i = 0; i < N; i++) {
+            area[i] = br.readLine().toCharArray();
         }
-        bfs(n, m);
-        System.out.println(dist[n - 1][m - 1]);
+        System.out.println(bfs());
     }
 
-    private static void bfs(int n, int m) {
-        Queue<Pair_2178> q = new LinkedList<>();
-        q.add(new Pair_2178(0, 0));
+    private static int bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] dist = new int[N][M];
+        queue.offer(new int[]{0, 0});
         dist[0][0] = 1;
-        while (!q.isEmpty()) {
-            Pair_2178 p = q.remove();
-            int x = p.x;
-            int y = p.y;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            if (cur[0] == N - 1 && cur[1] == M - 1) return dist[N - 1][M - 1];
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    if (dist[nx][ny] == 0 && a[nx][ny] == 1) {
-                        q.add(new Pair_2178(nx, ny));
-                        dist[nx][ny] = dist[x][y] + 1;
-                    }
-                }
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if (dist[nx][ny] > 0 || area[nx][ny] == '0') continue;
+                queue.offer(new int[]{nx, ny});
+                dist[nx][ny] = dist[cur[0]][cur[1]] + 1;
             }
         }
+        return 0;
     }
 }
