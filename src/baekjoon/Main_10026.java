d5e457d47fcb22ca1fdc76752b492717558eb59b
이유리 <dbf5156@naver.com>
2022-02-23 18:32:44 +0900

    [baekjoon] 10026

--- src/baekjoon/Main_10026.java
+++ src/baekjoon/Main_10026.java
@@ -1,51 +1,52 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
+// 소요 시간 : 92 ms
+// 메모리 사용량 : 12320 kb
 public class Main_10026 {
-    static private int[] dx = {0, 0, 1, -1};
-    static private int[] dy = {1, -1, 0, 0};
+    private static int N, count;
+    private static boolean[][] visited;
+    private static int[] dx = {-1, 1, 0, 0};
+    private static int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String[][] a1 = new String[n][n];
-        String[][] a2 = new String[n][n];
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split("");
-            for (int j = 0; j < n; j++) {
-                if (input[j].equals("R") || input[j].equals("G")) a2[i][j] = "R";
-                else a2[i][j] = input[j];
-                a1[i][j] = input[j];
+        N = Integer.parseInt(br.readLine());
+        char[][] area = new char[N][];
+        char[][] area2 = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            area[i] = br.readLine().toCharArray();
+            for (int j = 0; j < N; j++) {
+                area2[i][j] = (area[i][j] == 'R') ? 'G' : area[i][j];
             }
         }
-        System.out.println(findCount(a1, n) + " " + findCount(a2, n));
+        System.out.println(getNumArea(area) + " " + getNumArea(area2));
     }
 
-    private static int findCount(String[][] a, int n) {
-        boolean[][] check = new boolean[n][n];
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!check[i][j]) {
-                    dfs(a, check, i, j, n);
-                    count++;
-                }
+    private static int getNumArea(char[][] area) {
+        visited = new boolean[N][N];
+        count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (visited[i][j]) continue;
+                dfs(i, j, area);
+                count++;
             }
         }
         return count;
     }
 
-    private static void dfs(String[][] a, boolean[][] check, int x, int y, int n) {
-        check[x][y] = true;
+    private static void dfs(int x, int y, char[][] area) {
+        visited[x][y] = true;
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-                if (a[nx][ny].equals(a[x][y]) && !check[nx][ny])
-                    dfs(a, check, nx, ny, n);
-            }
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            if (visited[nx][ny] || area[x][y] != area[nx][ny]) continue;
+            dfs(nx, ny, area);
         }
     }
 }
