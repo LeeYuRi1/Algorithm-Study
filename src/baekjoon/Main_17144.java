5757f71775d69c40b71538ceae886d6da7ee66c4
이유리 <dbf5156@naver.com>
2022-02-25 22:38:39 +0900

    [baekjoon] 17144

--- src/baekjoon/Main_17144.java
+++ src/baekjoon/Main_17144.java
@@ -1,95 +1,96 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+// 소요 시간 : 228 ms
+// 메모리 사용량 : 23380 kb
 public class Main_17144 {
-    public static void main(String[] args) throws Exception {
+    private static int R, C, point, area[][];
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s1 = br.readLine().split(" ");
-        int r = Integer.parseInt(s1[0]);
-        int c = Integer.parseInt(s1[1]);
-        int t = Integer.parseInt(s1[2]);
-        int[][] room = new int[r][c];
-        int point = 0;
-        for (int i = 0; i < r; i++) {
-            String[] s2 = br.readLine().split(" ");
-            for (int j = 0; j < c; j++) {
-                if (Integer.parseInt(s2[j]) == -1) point = i;
-                room[i][j] = Integer.parseInt(s2[j]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        area = new int[R][C];
+        for (int i = 0; i < R; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < C; j++) {
+                area[i][j] = Integer.parseInt(st.nextToken());
+                if (area[i][j] == -1) point = i;
             }
         }
-        while (t-- > 0) {
-            room = diffusion(room, r, c);
-            cumulative(room, point, r, c);
+        while (T-- > 0) {
+            step1();
+            step2(true, 0, point - 1, 0, point); // 위쪽
+            step2(false, 2, point, point, R); // 아래쪽
         }
-        int ans = 0;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (room[i][j] != -1) ans += room[i][j];
-            }
-        }
-        System.out.println(ans);
+        System.out.println(getSumAmount());
     }
 
     // 미세먼지 확산
-    private static int[][] diffusion(int[][] room, int r, int c) {
-        int[][] temp = new int[r][c];
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (room[i][j] == -1) temp[i][j] = -1;
-                else if (room[i][j] > 0) {
-                    temp[i][j] += room[i][j];
-                    for (int k = 0; k < 4; k++) {
-                        int nx = i + dx[k];
-                        int ny = j + dy[k];
-                        if (nx >= 0 && nx < r && ny >= 0 && ny < c) {
-                            if (room[nx][ny] != -1) {
-                                temp[nx][ny] += room[i][j] / 5;
-                                temp[i][j] -= room[i][j] / 5;
-                            }
-                        }
-                    }
+    private static void step1() {
+        int[][] temp = new int[R][C];
+        temp[point - 1][0] = -1;
+        temp[point][0] = -1;
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (area[i][j] <= 0) continue;
+                int count = 0;
+                int sum = area[i][j] / 5;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx < 0 || nx >= R || ny < 0 || ny >= C || area[nx][ny] == -1) continue;
+                    temp[nx][ny] += sum;
+                    count++;
                 }
+                temp[i][j] += area[i][j] - (sum * count);
             }
         }
-        return temp;
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                area[i][j] = temp[i][j];
+            }
+        }
     }
 
-    // 공기청정기 가동
-    private static void cumulative(int[][] room, int point, int r, int c) {
-        // 위쪽 - 반시계방향
-        point--;
-        for (int i = point - 1; i > 0; i--) {
-            room[i][0] = room[i - 1][0];
+    // 공기청정기 작동
+    private static void step2(boolean top, int ds, int p, int minX, int maxX) {
+        int d = ds;
+        int x = p, y = 0, cnt = 0;
+        while (cnt < 4) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx < minX || nx >= maxX || ny < 0 || ny >= C) {
+                if (top) d++;
+                else d = (d + 3) % 4;
+                cnt++;
+                continue;
+            }
+            area[x][y] = area[nx][ny];
+            x = nx;
+            y = ny;
         }
-        for (int i = 0; i < c - 1; i++) {
-            room[0][i] = room[0][i + 1];
-        }
-        for (int i = 0; i < point; i++) {
-            room[i][c - 1] = room[i + 1][c - 1];
-        }
-        for (int i = c - 1; i > 1; i--) {
-            room[point][i] = room[point][i - 1];
-        }
-        room[point][1] = 0;
+        area[p][0] = -1;
+        area[p][1] = 0;
+    }
 
-        // 아래쪽 - 시계방향
-        point++;
-        for (int i = point + 1; i < r - 1; i++) {
-            room[i][0] = room[i + 1][0];
+    private static int getSumAmount() {
+        int sum = 0;
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (area[i][j] != -1) sum += area[i][j];
+            }
         }
-        for (int i = 0; i < c - 1; i++) {
-            room[r - 1][i] = room[r - 1][i + 1];
-        }
-        for (int i = r - 1; i > point; i--) {
-            room[i][c - 1] = room[i - 1][c - 1];
-        }
-        for (int i = c - 1; i > 0; i--) {
-            room[point][i] = room[point][i - 1];
-        }
-        room[point][1] = 0;
+        return sum;
     }
 }
