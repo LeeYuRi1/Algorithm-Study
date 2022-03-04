5357691d21695f5aeadac06b51c121fb1c9355fe
이유리 <dbf5156@naver.com>
2022-03-05 05:16:09 +0900

    [baekjoon] 1647

--- src/baekjoon/Main_1647.java
+++ src/baekjoon/Main_1647.java
@@ -6,70 +6,71 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.StringTokenizer;
 
-class Pair_1647 implements Comparable<Pair_1647> {
-    int start;
-    int end;
+class Edge_1647 implements Comparable<Edge_1647> {
+    int from;
+    int to;
     int cost;
 
-    public Pair_1647(int start, int end, int cost) {
-        this.start = start;
-        this.end = end;
+    Edge_1647(int from, int to, int cost) {
+        this.from = from;
+        this.to = to;
         this.cost = cost;
     }
 
     @Override
-    public int compareTo(Pair_1647 o) {
+    public int compareTo(Edge_1647 o) {
         return this.cost - o.cost;
     }
 }
 
 public class Main_1647 {
-    private static int[] parent;
+    private static int N, parent[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input1 = br.readLine().split(" ");
-        int n = Integer.parseInt(input1[0]);
-        int m = Integer.parseInt(input1[1]);
-        List<Pair_1647> pairList = new ArrayList<>();
-        while (m-- > 0) {
-            String[] input2 = br.readLine().split(" ");
-            int a = Integer.parseInt(input2[0]);
-            int b = Integer.parseInt(input2[1]);
-            int c = Integer.parseInt(input2[2]);
-            pairList.add(new Pair_1647(a, b, c));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        List<Edge_1647> edgeList = new ArrayList<>();
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            edgeList.add(new Edge_1647(a, b, c));
         }
-        Collections.sort(pairList);
-        parent = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
+        parent = new int[N + 1];
+        makeSet();
+        Collections.sort(edgeList);
+        int count = 0, sum = 0, max = 0;
+        for (Edge_1647 e : edgeList) {
+            if (union(e.from, e.to)) {
+                sum += e.cost;
+                max = Math.max(max, e.cost);
+                if (++count == N - 1) break;
+            }
+        }
+        System.out.println(sum - max);
+    }
+
+    private static void makeSet() {
+        for (int i = 1; i <= N; i++) {
             parent[i] = i;
         }
-        int ans = 0;
-        int max = 0;
-        // 크루스칼 알고리즘 사용
-        for (int i = 0; i < pairList.size(); i++) {
-            Pair_1647 p = pairList.get(i);
-            if (find(p.start) == find(p.end)) continue;
-            union(p.start, p.end);
-            ans += p.cost;
-            max = p.cost;
-        }
-        System.out.println(ans - max);
     }
 
-    // union-find: 여러 노드가 존재할 때, 두개의 노드가 같은 집합에 있는지 확인하는 알고리즘
-    // find: x가 어떤 집합에 포함되어 있는지 찾음
-    private static int find(int x) {
+    private static int findSet(int x) {
         if (x == parent[x]) return x;
-        parent[x] = find(parent[x]); // 재귀로 집합의 root 노드를 찾음
-        return parent[x];
+        return parent[x] = findSet(parent[x]);
     }
 
-    // union: x와 y가 포함되어 있는 집합을 합침
-    private static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x != y) parent[y] = x;
+    private static boolean union(int x, int y) {
+        int rx = findSet(x);
+        int ry = findSet(y);
+        if (rx == ry) return false;
+        parent[ry] = rx;
+        return true;
     }
 }
