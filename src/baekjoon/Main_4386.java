68bc4c79ba3b7da298ac90b1ecb5bf1fd671bdee
이유리 <dbf5156@naver.com>
2022-03-04 22:01:05 +0900

    [baekjoon] 4386

--- src/baekjoon/Main_4386.java
+++ src/baekjoon/Main_4386.java
@@ -1,71 +1,50 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-class Pair implements Comparable<Pair> {
-    int start;
-    int end;
-    double dist;
-
-    public Pair(int start, int end, double dist) {
-        this.start = start;
-        this.end = end;
-        this.dist = dist;
-    }
-
-    @Override
-    public int compareTo(Pair o) {
-        return Double.compare(this.dist, o.dist);
-    }
-}
+import java.util.StringTokenizer;
 
 public class Main_4386 {
-    private static int[] parent;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        double[][] start = new double[n][2];
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
-            start[i][0] = Double.parseDouble(input[0]);
-            start[i][1] = Double.parseDouble(input[1]);
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        double[][] stars = new double[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            stars[i][0] = Double.parseDouble(st.nextToken());
+            stars[i][1] = Double.parseDouble(st.nextToken());
         }
-        List<Pair> pairList = new ArrayList<>();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                double dist = Math.sqrt(Math.pow(start[i][0] - start[j][0], 2) + Math.pow(start[i][1] - start[j][1], 2));
-                pairList.add(new Pair(i, j, dist));
+        double[][] dist = new double[N][N];
+        double[] minEdge = new double[N];
+        boolean[] visited = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                double d = Math.sqrt(Math.pow(Math.abs(stars[i][0] - stars[j][0]), 2) + Math.pow(Math.abs(stars[i][1] - stars[j][1]), 2));
+                dist[i][j] = dist[j][i] = d;
+            }
+            minEdge[i] = Integer.MAX_VALUE;
+        }
+
+        double ans = 0;
+        minEdge[0] = 0;
+        for (int i = 0; i < N; i++) {
+            double min = Double.MAX_VALUE;
+            int minV = 0;
+            for (int j = 0; j < N; j++) {
+                if (visited[j] || minEdge[j] >= min) continue;
+                min = minEdge[j];
+                minV = j;
+            }
+            visited[minV] = true;
+            ans += min;
+
+            for (int j = 0; j < N; j++) {
+                if (visited[j] || dist[minV][j] == 0) continue;
+                if (minEdge[j] > dist[minV][j]) minEdge[j] = dist[minV][j];
             }
         }
-        Collections.sort(pairList);
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-        double ans = 0;
-        for (int i = 0; i < pairList.size(); i++) {
-            Pair p = pairList.get(i);
-            if (find(p.start) == find(p.end)) continue;
-            union(p.start, p.end);
-            ans += p.dist;
-        }
         System.out.println(String.format("%.2f", ans));
-    }
-
-    private static int find(int x) {
-        if (x == parent[x]) return x;
-        parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    private static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x != y) parent[y] = x;
     }
 }
