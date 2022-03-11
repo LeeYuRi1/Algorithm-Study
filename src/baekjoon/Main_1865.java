e1472e0b4dd5e153353a6d980a2623f232425c4f
이유리 <dbf5156@naver.com>
2022-03-12 01:28:54 +0900

    [baekjoon] 1865

--- src/baekjoon/Main_1865.java
+++ src/baekjoon/Main_1865.java
@@ -1,62 +1,65 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 class Edge_1865 {
-    int from;
-    int to;
-    int cost;
+    int from, to, time;
 
-    Edge_1865(int from, int to, int cost) {
+    Edge_1865(int from, int to, int time) {
         this.from = from;
         this.to = to;
-        this.cost = cost;
+        this.time = time;
     }
 }
 
 public class Main_1865 {
-    public static void main(String[] args) throws Exception {
+    private static int N;
+    private static List<Edge_1865> list;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
+        int TC = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        while (tc-- > 0) {
-            String[] input = br.readLine().split(" ");
-            int n = Integer.parseInt(input[0]);
-            int m = Integer.parseInt(input[1]);
-            int w = Integer.parseInt(input[2]);
-            ArrayList<Edge_1865> g = new ArrayList<>();
-            for (int i = 0; i < m + w; i++) {
-                String[] input2 = br.readLine().split(" ");
-                int s = Integer.parseInt(input2[0]);
-                int e = Integer.parseInt(input2[1]);
-                int t = Integer.parseInt(input2[2]);
-                if (i < m) {
-                    g.add(new Edge_1865(s, e, t));
-                    g.add(new Edge_1865(e, s, t));
+        while (TC-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            list = new ArrayList<>();
+            for (int i = 0; i < M + W; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+                if (i < M) {
+                    list.add(new Edge_1865(a, b, c));
+                    list.add(new Edge_1865(b, a, c));
                 } else {
-                    g.add(new Edge_1865(s, e, -t));
+                    list.add(new Edge_1865(a, b, -c));
                 }
             }
-            sb.append(bellmanFord(g, n, m) + "\n");
+            sb.append(bellmanFord()).append("\n");
         }
         System.out.println(sb);
     }
 
-    private static String bellmanFord(ArrayList<Edge_1865> g, int n, int m) {
-        int[] d = new int[n + 1];
-        Arrays.fill(d, 100000000);
-        d[1] = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < g.size(); j++) {
-                int s = g.get(j).from;
-                int e = g.get(j).to;
-                int t = g.get(j).cost;
-                if (d[e] <= d[s] + t) continue;
-                d[e] = d[s] + t;
-                if (i == n) return "YES";
+    private static String bellmanFord() {
+        long[] ans = new long[N + 1];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        ans[1] = 0;
+        for (int i = 1; i <= N; i++) {
+            for (Edge_1865 e : list) {
+//                if (ans[e.from] == Integer.MAX_VALUE) continue; // 최단거리가 아닌 사이클만 찾아야하기 때문!
+                if (ans[e.to] > ans[e.from] + e.time) {
+                    ans[e.to] = ans[e.from] + e.time;
+                    if (i == N) return "YES";
+                }
             }
         }
         return "NO";
